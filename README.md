7bb335f4cb2ab1082854193053a305e3440edd76
Virendra1306 <virendrasinghnegi0123@gmail.com>
2023-11-17 23:07:13 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ViewFlux
+ Explore a seamless video streaming experience with our ReactJS and Tailwind CSS- powered website,featuring a user-friendly home feed. Effortlessly navigate through a dynamic feed using a robustsearch bar andenjoy personalized video recommendations with leveraging APIs
---
+++ src/utils/api.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const BASE_URL = "https://youtube138.p.rapidapi.com";
+
+const options = {
+    params: { hl: "en", gl: "US" },
+    headers: {
+        "X-RapidAPI-Key":
+            process.env.REACT_APP_YOUTUBE_API_KEY || "881513f26bmsh632e707a40f62a2p19840ejsn2a12083e37ce",
+        "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
+    },
+};
+
+export const fetchDataFromApi = async (url) => {
+    const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+    return data;
+};
